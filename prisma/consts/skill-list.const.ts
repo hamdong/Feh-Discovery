07eb5c517a26289c7737bdcdd5c1591fb41b10c0
hamdong <mrzombielover@gmail.com>
2023-09-03 13:54:33 -0400

    reduced skill list const concatentation

--- prisma/consts/skill-list.const.ts
+++ prisma/consts/skill-list.const.ts
@@ -1,11 +1,7 @@
-import * as s from '../models';
+import * as skill from '../models';
 import { ISkill } from '../models/skill.interface';
 
-export const skills: ISkill[] = [].concat(
-  s.ardSkills,
-  s.blowSkills,
-  s.flatStatSkills,
-  s.fortressSkills,
-  s.stanceSkills,
-  s.shieldSkills,
+export const skills: ISkill[] = Object.values(skill).reduce(
+  (acc, arr) => acc.concat(arr),
+  [],
 );
